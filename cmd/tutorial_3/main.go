d46c5d561a4e75ae22269229b9da8cbbc89ffe2d
Shriyans S Sahoo <shriyans.s.sahoo@gmail.com>
2026-08-21 09:18:43 +0530

    learning tut 3

--- cmd/tutorial_3/main.go
+++ cmd/tutorial_3/main.go
@@ -3,9 +3,21 @@ package main
 import "fmt"
 
 func main(){
-	printMe()
+	var printValue string = "Hello world"
+	printMe(printValue)
+
+	var numerator int = 10
+	var denominator int = 2
+	var result int = intDivision(numerator, denominator)
+	fmt.Println(result)	
+}
+       
+func printMe(printValue string) {
+	fmt.Println(printValue)
 }
 
-func printMe(){
-	fmt.Println("Hello world")
+func intDivision(numerator int, denominator int)(int, int) {
+	var result int= numerator / denominator
+	var remainder int = numerator % denominator
+	return result, remainder
 }
