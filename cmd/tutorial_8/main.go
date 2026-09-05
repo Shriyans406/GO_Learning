89e9d0f0f2fc117f9f780fc107b592f97697559b
Shriyans S Sahoo <shriyans.s.sahoo@gmail.com>
2026-09-05 16:18:42 +0530

    goroutines log and save

--- cmd/tutorial_8/main.go
+++ cmd/tutorial_8/main.go
@@ -4,7 +4,7 @@ import{
 	"fmt"
 	"math/rand"
 	"time"
-
+	"sync"
 }
 
 var wg = sync.WaitGroup{}
@@ -21,11 +21,33 @@ func main(){
 	fmt.Printf("\nTime taken for sequential execution: %v", time.Since(t0))
 	fmt.Printf("\nResults: %v", results)
 
+	// func dbCall(i int){
+	// 	var delay float32 = rand.Float32()*2000
+	// 	time.Sleep(time.Duration(delay) * time.Millisecond)
+	// 	fmt.Println(dbData[i])
+	// 	results=append(results, dbData[i])
+	// 	wg.Done()
+	// }
+
 	func dbCall(i int){
-		var delay float32 = rand.Float32()*2000
-		time.Sleep(time.Duration(delay) * time.Millisecond)
-		fmt.Println(dbData[i])
+		var delay float32= 2000
+		time.Sleep(time.Duration(delay)*time.Millisecond)
+		fmt.Println("The result from the database is :", dbData[i])
+		m.Lock()
 		results=append(results, dbData[i])
+		m.Unlock()
 		wg.Done()
+	}
+
+	func save(result string){
+		m.Lock()
+		results=append(results, result)
+		m.Unlock()
+	}
+
+	func log(){
+		m.RLock()
+		fmt.Println("The results are :", results)
+		m.RUnlock()
 	}
 }
