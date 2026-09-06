cb77713ac62c537328f7857bf74f5fd298a2e600
Shriyans S Sahoo <shriyans.s.sahoo@gmail.com>
2026-09-06 19:22:12 +0530

    expaned channels

--- main.go
+++ main.go
@@ -1,23 +1,49 @@
 package main
 
-import "fmt"
-
-func main() {
-	var c=make(chan int)
-	c <- 1
-	var i= <- c
-	fmt.Println(i)
-
-	go process(c)
-	for i:= range c{
-		fmt.Println(i)
-	}
+import {
+	"fmt"
+	"math/rand"
+	"time"
 }
 
-func process(c chan int){
-	c <- 123
+// func main() {
+// 	var c=make(chan int)
+// 	c <- 1
+// 	var i= <- c
+// 	fmt.Println(i)
 
-	for i:=0; i<10; i++{
-		c <- 1
+// 	go process(c)
+// 	for i:= range c{
+// 		fmt.Println(i)
+// 	}
+// }
+
+// func process(c chan int){
+// 	c <- 123
+
+// 	for i:=0; i<10; i++{
+// 		c <- 1
+// 	}
+// } 
+
+var MAX_CHICKEN_PRICE=5
+
+func main(){
+	var chickenChannel=make(chan string)
+	var websites=[]string{"walmart.com", "costco.com", "wholefoods.com"}
+	for i:= range websites{
+		go checkChickenPricea(websites[i], chickenChannel)
+	}
+	sendMessage(chickenChannel)
+}
+
+func checkChickenPrices(website string, chickenChannel chan string){
+	for{
+		time.Sleep(time.Second*1)
+		var chickenPrice=rand.Float32()*20
+		if chickenPrice<=MAX_CHICKEN_PRICE{
+			chickenChannel <- website
+			break
+		}
 	}
 }
